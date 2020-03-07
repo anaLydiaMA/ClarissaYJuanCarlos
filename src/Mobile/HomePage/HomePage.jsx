51cb1aa4b56984b30d258b9be8b9568365c9c2ec
la Ana <analydia.ma@hotmail.com>
2020-03-06 19:46:08 -0600

    content details added

--- src/Mobile/HomePage/HomePage.jsx
+++ src/Mobile/HomePage/HomePage.jsx
@@ -2,8 +2,10 @@ import React,{Component} from 'react';
 import './HomePage.css';
 import image1 from '../../img/1.JPG';
 import image2 from '../../img/2.JPG';
+import image3 from '../../img/3.JPG';
 import image4 from '../../img/4.JPG';
 import image5 from '../../img/5.JPG';
+import church from '../../img/church.jpg';
 
 class HomePage extends Component{
   constructor(props) {
@@ -21,7 +23,7 @@ class HomePage extends Component{
         <h1 className="Dancing white title no-gutters">Clarissa</h1>
         <h1 className="Dancing white subtitle no-gutters">y</h1>
         <h1 className="Dancing white title no-gutters">Juan Carlos</h1>
-        <h4 className="Lato white font-weight-light"> NUESTRO 25 ANIVERSARIO</h4>
+        <h4 className="Lato white no-gutters font-weight-light"> NUESTRO 25 ANIVERSARIO</h4>
       </div>
       <div id="carrousel" className="img-container">
         <h4 className="Lato white font-weight-light no-gutters caption-space">Hace 25 años nos tomamos de la mano y empezamos esta aventura.</h4>
@@ -29,21 +31,38 @@ class HomePage extends Component{
         <h4 className="Lato white font-weight-light no-gutters caption-space">Hemos crecido juntos a través de las dificultades.</h4>
         <img src={image2} alt="CJC" className="image-style"/>
         <h4 className="Lato white font-weight-light no-gutters caption-space">Hemos aprendido del otro.</h4>
-        <img src={image4} alt="CJC" className="image-style"/>
+        <img src={image3} alt="CJC" className="image-style"/>
         <h4 className="Lato white font-weight-light no-gutters caption-space">Y nunca hemos renunciado al amor que nos unió.</h4>
         <img src={image5} alt="CJC" className="image-style"/>
       </div>
 
-      <div id="location" className="loc-container map-style">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d481935.6597572109!2d-99.1526134!3d19.3204968!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85cdff96438ec639%3A0x9c7dd901f7541a49!2sSuntory%20Pedregal!5e0!3m2!1ses!2sus!4v1583521408011!5m2!1ses!2sus" frameborder="0" style={{'border':0}} allowfullscreen=""></iframe>
+      <div id="church-information" className="inv-container">
+        <h1 className="Dancing title subtitle">Familia</h1>
+        <h4 className="Lato font-weight-light description-text no-gutters"> Nos gustaría mucho que nos acompañaran a la Santa Misa el día 1ro de Mayo a las 13:00 horas.</h4>
+        <h4 className="Lato font-weight-light description-text"> En la que renovaremos nuestras promesas matrimoniales en compañía de nuestros hijos Ana Lydia, Luis Enrique y Juan Carlos.</h4>
+        <h4 className="Lato font-weight-light description-text"> En compañia de nuestros padrinos de hace 25 años, Pedro y Linda.</h4>
+        <img src={church} alt="CJC" className="image-normal"/>
       </div>
 
-      <div id="invitation-information" className="inv-container">
-        <h4 className="Lato font-weight-light"> Por esta razon te invitamos blah blah blah</h4>
+      <div id="church-location" className="loc-container map-style">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3733.842249029923!2d-103.32657958455292!3d20.635285886212902!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8428b2457f47f779%3A0x5f854671f72e0f41!2sParroquia%20Mar%C3%ADa%20Reina%20del%20Rosario!5e0!3m2!1ses!2smx!4v1583539579615!5m2!1ses!2smx" frameborder="0" style={{'border':0}} allowfullscreen=""></iframe>
+      </div>
+
+      <div id="party-information" className="inv-container">
+        <h1 className="Dancing title subtitle">-</h1>
+        <h4 className="Lato font-weight-light description-text">A partir de las 14:30 horas los esperamos en el Jardín de Eventos San Joaquin, para compartir los alimentos.</h4>
+        <img src={church} alt="CJC" className="image-normal"/>
+      </div>
+
+      <div id="party-location" className="loc-container map-style">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4889.703307381015!2d-103.25116800159473!3d20.603911199400702!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8428b4960999976f%3A0xe6dcbd13c3b0535a!2sJardin%20de%20Eventos%20San%20Joaquin!5e0!3m2!1ses!2smx!4v1583544204890!5m2!1ses!2smx" frameborder="0" style={{'border':0}} allowfullscreen=""></iframe>
+      </div>
+
+      <div id="confirmation-button" className="inv-container space-on-text">
         <button type="button" className="btn-style" onClick={() => this.setState ({showConfirmation: !this.state.showConfirmation})} >CONFIRMAR ASISTENCIA</button>
         {
           this.state.showConfirmation?
-          <div id="test" className="test-container">Contenido</div>
+          <div id="test" className="test-container">Dress Code: CASUAL. NO NIÑOS</div>
           :null
         }
       </div>
@@ -53,9 +72,7 @@ class HomePage extends Component{
 
       </div>
     );
-
-}
-
+  }
 }
 
 export default HomePage;
